49bc8bf6ae620a5b67ca90890eb33a20f3502633
zer0 <mhasselwander@netlive-it.ch>
2025-09-24 12:00:42 +0200

    mf-core convert basic structure

--- mf-core/src/convert.rs
+++ mf-core/src/convert.rs
@@ -2,6 +2,8 @@ use std::fs::File;
 
 use crate::job::ConvertJob;
 
-pub fn convert(cj: ConvertJob) {
-    let mut input_file = File::open(cj.input);
+pub fn convert(cj: ConvertJob) -> Result<PathBuf, MeltforgeError> {
+    validate_job(&cj)?; // Validate
+    let out = convert_png_jpg(&cj)?; // Convert currently only png  to jpg will later be replaced with the plugin function
+    Ok(out) // Respond
 }
