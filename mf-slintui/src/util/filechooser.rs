21365c7955961f1f637ec38c2b1b4025184a1ffb
Zkk0 <max.hasselwander@outlook.com>
2026-01-26 18:20:14 +0100

    file chooser backend

---
+++ mf-slintui/src/util/filechooser.rs
@@ -0,0 +1,41 @@
+use mf_core::validation::error::{IoError, MeltforgeError};
+use native_dialog::{DialogBuilder, Error as DialogError, MessageLevel};
+use std::path::PathBuf;
+
+pub async fn open_dialog() -> Result<Option<PathBuf>, MeltforgeError> {
+    let path = DialogBuilder::file()
+        .set_location("~/Desktop")
+        .open_single_file()
+        .show()
+        .map_err(map_dialog_err)?;
+
+    let Some(path) = path else {
+        return Ok(None);
+    };
+
+    let yes = DialogBuilder::message()
+        .set_level(MessageLevel::Info)
+        .set_title("Do you want to open the file?")
+        .set_text(format!("{:#?}", path))
+        .confirm()
+        .spawn()
+        .await
+        .map_err(map_dialog_err)?;
+
+    if yes {
+        Ok(Some(path))
+    } else {
+        Ok(None)
+    }
+}
+
+fn map_dialog_err(err: DialogError) -> MeltforgeError {
+    let msg = match err {
+        DialogError::Io(e) => format!("native dialog I/O error: {}", e),
+        DialogError::Utf8(e) => format!("native dialog utf-8 error: {}", e),
+        DialogError::MissingDep => "native dialog dependency missing".to_string(),
+        DialogError::Killed(sig) => format!("native dialog subprocess killed: {:?}", sig),
+        DialogError::Other(e) => format!("native dialog error: {}", e),
+    };
+    MeltforgeError::Io(IoError::InvalidOutput(msg))
+}
