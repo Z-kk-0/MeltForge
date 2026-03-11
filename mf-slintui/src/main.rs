da656ac37750963bfc7a6c8e580509941c566a32
Zkk0 <max.hasselwander@outlook.com>
2026-03-11 11:58:03 +0100

    some changes

--- mf-slintui/src/main.rs
+++ mf-slintui/src/main.rs
@@ -1,9 +1,17 @@
 slint::include_modules!();
 mod services;
 mod util;
+use slint::{SharedString, VecModel};
+use util::filechooser;
 fn main() {
     let app = MainWindow::new().unwrap();
     app.set_app_version(slint::SharedString::from(env!("CARGO_PKG_VERSION")));
+    let files_model = Rc::new(VecModel::<SharedString>::default());
+    app.on_add_file_clicked(move || {
+        let OK(Some(input_file)) = filechooser::open_dialog().await {
+            
+        }
+    });
     app.run();
 }
 
