996c9feee98958b94154100d5c310f15c5213a66
Zkk0 <max.hasselwander@outlook.com>
2026-03-11 12:13:06 +0100

    ui getsa arry from filechooser

--- mf-slintui/src/main.rs
+++ mf-slintui/src/main.rs
@@ -1,17 +1,26 @@
 slint::include_modules!();
 mod services;
 mod util;
-use slint::{SharedString, VecModel};
+use slint::{ModelRc, SharedString, VecModel};
+use std::rc::Rc;
 use util::filechooser;
+
 fn main() {
     let app = MainWindow::new().unwrap();
-    app.set_app_version(slint::SharedString::from(env!("CARGO_PKG_VERSION")));
-    let files_model = Rc::new(VecModel::<SharedString>::default());
-    app.on_add_file_clicked(move || {
-        let OK(Some(input_file)) = filechooser::open_dialog().await {
-            
-        }
-    });
-    app.run();
-}
+    app.set_app_version(SharedString::from(env!("CARGO_PKG_VERSION")));
 
+    let files_model = Rc::new(VecModel::<SharedString>::default());
+    app.set_selected_files(ModelRc::from(files_model.clone()));
+
+    app.on_add_file_clicked(move || {
+        let files_model = files_model.clone();
+        slint::spawn_local(async move {
+            if let Ok(Some(input_file)) = filechooser::open_dialog().await {
+                files_model.push(SharedString::from(input_file.to_string_lossy().as_ref()));
+            }
+        })
+        .unwrap();
+    });
+
+    app.run().unwrap();
+}
--- mf-slintui/src/util/mod.rs
+++ mf-slintui/src/util/mod.rs
@@ -1,1 +1,1 @@
-mod filechooser;
+pub mod filechooser;
